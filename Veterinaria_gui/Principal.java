494a9b3b75b910e1ccee2bcff74151a84ae8d6a2
Ceball0s <105499762+Ceball0s@users.noreply.github.com>
2023-06-15 08:39:05 -0500

    VIsta fucnional

---
+++ Veterinaria_gui/Principal.java
@@ -0,0 +1,9 @@
+package Veterinaria_gui;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        //ventana ventanaP = new ventana();
+        Vista Vista_controlador = new Vista();
+        //ventanaP.setVisible(true); //Hacemos visible la ventana
+    }
+}
